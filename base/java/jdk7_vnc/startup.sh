60ed8e5839971ac714702f3c3328e30b255516e2
Sergey Leschenko <sleschenko@codenvy.com>
2014-05-29 15:54:35 +0300

    Changed user

--- base/java/jdk7_vnc/startup.sh
+++ base/java/jdk7_vnc/startup.sh
@@ -2,15 +2,6 @@
 
 mkdir /var/run/sshd
 
-# create an ubuntu user
-PASS=`pwgen -c -n -1 10`
-PASS=debian
-echo "User: debian Pass: $PASS"
-useradd --create-home --shell /bin/bash --user-group --groups adm,sudo debian
-echo "debian:$PASS" | chpasswd
-
-cp /tmp/application.jar /home/debian/application.jar
-
 /usr/bin/supervisord -c /supervisord.conf
 
 while [ 1 ]; do
