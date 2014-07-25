acd78f4db62013d011b07f6d067367de294644b1
Vladyslav Zhukovskii <vzhukovskii@codenvy.com>
2014-07-25 15:57:33 +0300

    CLDIDE-1929: Add dockerfile and run script for MySQL template

---
+++ base/java/jdk7_mysql/startup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source /home/user/.mysqlrc
+
+JAR=/home/user/application.jar
+EXEC_JAVA=/opt/jdk1.7.0_55/bin/java
+
+echo "Waiting for MySQL server initialize..."
+service mysql start > /dev/null
+
+if [ $? -eq 0 ] ; then
+    echo "MySQL server started."
+
+    if [ -e $JAR ] ; then
+        echo "Starting application."
+	$EXEC_JAVA -jar $JAR $ARGUMENTS
+	echo "Done."
+    else
+	echo "Executable jar application dosn't exist."
+    fi
+else
+    echo "Failed to start MySQL server."
+fi
