d2f15a1b76e76b7d808a4e398cf20abe86148990
Vladyslav Zhukovskii <vzhukovskii@codenvy.com>
2014-08-06 10:54:52 +0300

    CLDIDE-1954: Create template and docker image for Couchbase

---
+++ base/java/jdk7_couchbase/startup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+JAR=/home/user/application.jar
+EXEC_JAVA=/opt/jdk1.7.0_55/bin/java
+
+untilsuccessful() {
+    "$@"
+    while [ $? -ne 0 ]
+    do
+        echo Retrying...
+        sleep 1
+        "$@"
+    done
+}
+
+echo "Waiting for Couchbase server initialize..."
+service couchbase-server start &>/dev/null
+
+if [ $? -eq 0 ] ; then
+    echo "Couchbase server started."
+
+    untilsuccessful /opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091 \
+    --cluster-init-username=Administrator \
+    --cluster-init-password=password \
+    --cluster-init-ramsize=512 &>/dev/null
+
+    echo "Cluster inited with username 'Administrator' and password 'password'."
+
+    untilsuccessful /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 \
+    --bucket=default \
+    --bucket-type=couchbase \
+    --bucket-ramsize=128 \
+    --bucket-replica=0 \
+    -u Administrator \
+    -p password \
+    --wait &>/dev/null
+
+    echo "Default bucket created."
+
+    if [ -e $JAR ] ; then
+    echo "Starting application."
+    $EXEC_JAVA -jar $JAR $ARGUMENTS
+    echo "Done."
+    else
+    echo "Executable jar application dosn't exist."
+    fi
+else
+    echo "Failed to start Couchbase server."
+fi
