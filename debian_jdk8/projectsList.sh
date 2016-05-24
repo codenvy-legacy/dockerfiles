5ac2ff2d473b6e75c836ef161e9e4f96f358080a
Eugene Ivantsov <eivantsov@codenvy.com>
2016-05-24 17:42:05 +0300

    Update licence header, remove cbuild

--- debian_jdk8/projectsList.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-COMMON_PROJECTS=( che-parent che-depmgt che-core che-plugins dashboard )
-HOSTED_ONLY_PROJECTS=( codenvy-depmgt hosted-infrastructure plugins odyssey factory )
-ALL_PROJECTS=( ${COMMON_PROJECTS[@]} che ${HOSTED_ONLY_PROJECTS[@]} cloud-ide platform-api-client-java cli cdec onpremises )
-IDE_SDK_PROJECTS=( ${COMMON_PROJECTS[@]} che )
-IDE_HOSTED_PROJECTS=( ${COMMON_PROJECTS[@]} ${HOSTED_ONLY_PROJECTS[@]} cloud-ide )
-IDE_ONPREM_PROJECTS=( ${IDE_HOSTED_PROJECTS[@]} platform-api-client-java cli cdec onpremises )
